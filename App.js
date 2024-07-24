9a5108ef4712578c3e5064da13366eac84bfc7d3
1zzu <wahyuizzudin20@gmail.com>
2024-07-24 10:13:23 +0700

    add react navigation

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  function HomeScreen() {
+    return(
+      <>
+        <View>
+          <Text>Ini adalah tampilan home</Text>
+        </View>
+      </>
+    )
+  }
+
+  function AppScreen() {
+    return(
+      <>
+        <View className="flex-1 bg-slate-200 justify-center items-center ">
+          <Text className="text-blue-300">Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </>
+    )
+  }
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="App" component={AppScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
