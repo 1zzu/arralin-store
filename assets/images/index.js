fe9585021ce0bea0294dff8879f0503c50373e4b
1zzu <wahyuizzudin20@gmail.com>
2024-07-30 09:25:14 +0700

    add banner

--- assets/images/index.js
+++ assets/images/index.js
@@ -2,10 +2,16 @@ import Iklan1 from './iklan.png'
 import BgHead from './bg-header.png'
 import BgHead1 from './bg-header-1.png'
 import Header2 from './header-2.jpeg'
+import Image1 from './image1.png'
+import Image2 from './image2.png'
+import Image3 from './image3.png'
 
 export {
     Iklan1,
     BgHead,
     BgHead1,
-    Header2
+    Header2,
+    Image1,
+    Image2,
+    Image3,
 }
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -5,12 +5,12 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import { styled } from 'nativewind'
 
 import { StatusBar } from 'expo-status-bar'
-import { Header2, Iklan1 } from '../../assets/images'
+import { Image1, Image2, Image3 } from '../../assets/images'
 
 const StyledView = styled(View)
 
 export default function HomeScreen() {
-    const [ refreshing, setRefreshing ] = useState(false)
+    const [refreshing, setRefreshing] = useState(false)
 
     const onRefresh = useCallback(() => {
         setRefreshing(true)
@@ -20,33 +20,37 @@ export default function HomeScreen() {
     }, [])
 
     return (
-        <SafeAreaView className="flex-1">
+        <SafeAreaView className="flex-1 bg-white">
             <StatusBar backgroundColor='white' />
-            <View>
-                <View className="bg-white h-18 rounded-b-3xl relative">
-                    <View className="mt-4 mb-4 mx-4">
-                        <View className="flex flex-row items-center gap-2">
-                            <StyledView className="flex-row flex-1 items-center bg-gray-100 border-gray-300 rounded-3xl p-1">
-                                <Ionicons name="search-outline" size={22} style={{ position: 'absolute', left: 12, color: '#6D6D6D' }} />
-                                <TextInput className="flex-1 pl-9 h-8 text-base" placeholder="Cari di Arralin Store" />
-                            </StyledView>
-                            <Ionicons name="notifications-outline" size={28} style={{ color: '#03346E' }} />
-                            <Ionicons name="cart-outline" size={28} style={{ color: '#03346E' }} />
-                        </View>
+            <View className="bg-white h-18 rounded-b-2xl -mb-3 z-10">
+                <View className="mt-8 mb-4 mx-4">
+                    <View className="flex flex-row items-center gap-2">
+                        <StyledView className="flex-row flex-1 items-center bg-gray-100 border-gray-300 rounded-3xl p-1">
+                            <Ionicons name="search-outline" size={22} style={{ position: 'absolute', left: 12, color: '#6D6D6D' }} />
+                            <TextInput className="flex-1 pl-9 h-8 text-base" placeholder="Cari di Arralin Store" />
+                        </StyledView>
+                        <Ionicons name="notifications-outline" size={28} style={{ color: '#03346E' }} />
+                        <Ionicons name="cart-outline" size={28} style={{ color: '#03346E' }} />
                     </View>
                 </View>
-                <ScrollView
-                    contentContainerStyle={styles.scrollView}
-                    showsVerticalScrollIndicator={false}
-                    refreshControl={
-                        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-                    }>
-                        <StyledView>
-                            <Image source={Header2} />
-                        </StyledView>
-                </ScrollView>
             </View>
-
+            <ScrollView
+                contentContainerStyle={styles.scrollView}
+                showsVerticalScrollIndicator={false}
+                refreshControl={
+                    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+                }>
+                    <View>
+                        <ScrollView horizontal showsHorizontalScrollIndicator={false} >
+                            <Image source={Image1} className="w-screen h-56" />
+                            <Image source={Image2} className="w-screen h-56" />
+                            <Image source={Image3} className="w-screen h-56" />
+                        </ScrollView>
+                    </View>
+                    <View>
+                        
+                    </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
